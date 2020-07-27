09fd9756628a5b5df9616cc4205f08971497da74
Siddharth Vaish <sidvaish97@gmail.com>
2020-07-28 03:04:02 +0530

    action token add

---
+++ openmrs-keycloak-smart-auth-handler/src/main/java/org/openmrs/contrib/keycloak/smart/auth/ExternalAppAuthenticatorFactory.java
@@ -0,0 +1,103 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.contrib.keycloak.smart.auth;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+import java.util.Arrays;
+import java.util.List;
+import static org.keycloak.provider.ProviderConfigProperty.STRING_TYPE;
+
+/**
+ *
+ * @author hmlnarik
+ */
+public class ExternalAppAuthenticatorFactory implements AuthenticatorFactory {
+
+    private static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+            AuthenticationExecutionModel.Requirement.REQUIRED,
+            AuthenticationExecutionModel.Requirement.DISABLED
+    };
+
+    public static final String CONFIG_APPLICATION_ID = "application-id";
+    public static final String CONFIG_EXTERNAL_APP_URL = "external-application-url";
+
+    public static final String ID = "external-application-authenticator";
+
+    @Override
+    public String getDisplayType() {
+        return "External Application Authenticator";
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return null;
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return true;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return REQUIREMENT_CHOICES;
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "External Application Authenticator";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        ProviderConfigProperty rep1 = new ProviderConfigProperty(CONFIG_APPLICATION_ID, "Application ID",
+          "Application ID sent in the token",
+          STRING_TYPE, ExternalAppAuthenticator.DEFAULT_APPLICATION_ID);
+
+        ProviderConfigProperty rep2 = new ProviderConfigProperty(CONFIG_EXTERNAL_APP_URL, "External Application URL",
+          "URL of the application to redirect to. It has to contain token position marked with \"{TOKEN}\" (without quotes).",
+          STRING_TYPE, ExternalAppAuthenticator.DEFAULT_EXTERNAL_APP_URL);
+        
+        return Arrays.asList(rep1, rep2);
+    }
+
+    @Override
+    public Authenticator create(KeycloakSession keycloakSession) {
+        return new ExternalAppAuthenticator();
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+}
--- openmrs-keycloak-smart-auth-handler/src/main/java/org/openmrs/contrib/keycloak/smart/auth/token/ExternalApplicationNotificationActionTokenHandler.java
+++ openmrs-keycloak-smart-auth-handler/src/main/java/org/openmrs/contrib/keycloak/smart/auth/token/ExternalApplicationNotificationActionTokenHandler.java
@@ -12,27 +12,24 @@ package org.openmrs.contrib.keycloak.smart.auth.token;
 import org.keycloak.Config.Scope;
 import org.keycloak.TokenVerifier;
 import org.keycloak.TokenVerifier.Predicate;
+import org.keycloak.authentication.AuthenticationProcessor;
 import org.keycloak.authentication.actiontoken.AbstractActionTokenHander;
 import org.keycloak.authentication.actiontoken.*;
 import org.keycloak.common.VerificationException;
 import org.keycloak.common.util.Base64;
-import org.keycloak.events.Errors;
-import org.keycloak.events.EventBuilder;
-import org.keycloak.events.EventType;
-import org.keycloak.forms.login.LoginFormsProvider;
-import org.keycloak.models.UserModel;
+import org.keycloak.events.*;
 import org.keycloak.representations.JsonWebToken;
-import org.keycloak.services.managers.AuthenticationManager;
 import org.keycloak.services.managers.AuthenticationSessionManager;
 import org.keycloak.services.messages.Messages;
 import org.keycloak.sessions.AuthenticationSessionCompoundId;
 import org.keycloak.sessions.AuthenticationSessionModel;
 import java.io.IOException;
-import java.util.Collections;
 import javax.crypto.spec.SecretKeySpec;
 import javax.ws.rs.core.Response;
 import org.jboss.logging.Logger;
-import org.openmrs.contrib.keycloak.smart.auth.action.RedirectToExternalApplication;
+import org.openmrs.contrib.keycloak.smart.auth.token.ExternalApplicationNotificationActionToken;
+
+import static org.keycloak.services.resources.LoginActionsService.AUTHENTICATE_PATH;
 
 /**
  * Action token handler for verification of e-mail address.
@@ -42,27 +39,29 @@ public class ExternalApplicationNotificationActionTokenHandler extends AbstractA
 
     public static final String QUERY_PARAM_APP_TOKEN = "app-token";
 
+    public static final String INITIATED_BY_ACTION_TOKEN_EXT_APP = "INITIATED_BY_ACTION_TOKEN_EXT_APP";
+
     private SecretKeySpec hmacSecretKeySpec = null;
 
     public ExternalApplicationNotificationActionTokenHandler() {
         super(
-          ExternalApplicationNotificationActionToken.TOKEN_TYPE,
-          ExternalApplicationNotificationActionToken.class,
-          Messages.INVALID_REQUEST,
-          EventType.EXECUTE_ACTION_TOKEN,
-          Errors.INVALID_REQUEST
+                ExternalApplicationNotificationActionToken.TOKEN_TYPE,
+                ExternalApplicationNotificationActionToken.class,
+                Messages.INVALID_REQUEST,
+                EventType.EXECUTE_ACTION_TOKEN,
+                Errors.INVALID_REQUEST
         );
     }
 
     private boolean isApplicationTokenValid(
-      ExternalApplicationNotificationActionToken token,
-      ActionTokenContext<ExternalApplicationNotificationActionToken> tokenContext
+            ExternalApplicationNotificationActionToken token,
+            ActionTokenContext<ExternalApplicationNotificationActionToken> tokenContext
     ) throws VerificationException {
         String appTokenString = tokenContext.getUriInfo().getQueryParameters().getFirst(QUERY_PARAM_APP_TOKEN);
 
         TokenVerifier.create(appTokenString, JsonWebToken.class)
-          .secretKey(hmacSecretKeySpec)
-          .verify();
+                .secretKey(hmacSecretKeySpec)
+                .verify();
 
         return true;
     }
@@ -70,65 +69,34 @@ public class ExternalApplicationNotificationActionTokenHandler extends AbstractA
     @Override
     public Predicate<? super ExternalApplicationNotificationActionToken>[] getVerifiers(ActionTokenContext<ExternalApplicationNotificationActionToken> tokenContext) {
         return TokenUtils.predicates(
-          // Check that the app token is set in query parameters
-          t -> tokenContext.getUriInfo().getQueryParameters().getFirst(QUERY_PARAM_APP_TOKEN) != null,
+                // Check that the app token is set in query parameters
+                t -> tokenContext.getUriInfo().getQueryParameters().getFirst(QUERY_PARAM_APP_TOKEN) != null,
 
-          // Validate correctness of the app token
-          t -> isApplicationTokenValid(t, tokenContext)
+                // Validate correctness of the app token
+                t -> isApplicationTokenValid(t, tokenContext)
         );
+    }
+
+    @Override
+    public Response handleToken(ExternalApplicationNotificationActionToken token, ActionTokenContext<ExternalApplicationNotificationActionToken> tokenContext) {
+        // Continue with the authenticator action
+        tokenContext.getAuthenticationSession().setAuthNote(INITIATED_BY_ACTION_TOKEN_EXT_APP, "true");
+        return tokenContext.processFlow(true, AUTHENTICATE_PATH, tokenContext.getRealm().getBrowserFlow(), null, new AuthenticationProcessor());
     }
 
     private static final Logger LOG = Logger.getLogger(ExternalApplicationNotificationActionTokenHandler.class);
 
     @Override
     public String getAuthenticationSessionIdFromToken(ExternalApplicationNotificationActionToken token, ActionTokenContext<ExternalApplicationNotificationActionToken> tokenContext,
-      AuthenticationSessionModel currentAuthSession) {
+            AuthenticationSessionModel currentAuthSession) {
         // always join current authentication session
         final String id = currentAuthSession == null
-          ? null
-          : AuthenticationSessionCompoundId.fromAuthSession(currentAuthSession).getEncodedId();
+                ? null
+                : AuthenticationSessionCompoundId.fromAuthSession(currentAuthSession).getEncodedId();
 
         LOG.infof("Returning %s", id);
 
         return id;
-    }
-
-    @Override
-    public Response handleToken(ExternalApplicationNotificationActionToken token, ActionTokenContext<ExternalApplicationNotificationActionToken> tokenContext) {
-        UserModel user = tokenContext.getAuthenticationSession().getAuthenticatedUser();
-        EventBuilder event = tokenContext.getEvent();
-
-        AuthenticationSessionModel authSession = tokenContext.getAuthenticationSession();
-
-        // Update user according to the claims in the application token
-        String appTokenString = tokenContext.getUriInfo().getQueryParameters().getFirst(QUERY_PARAM_APP_TOKEN);
-        try {
-            JsonWebToken appToken = TokenVerifier.create(appTokenString, JsonWebToken.class).getToken();
-            appToken.getOtherClaims()
-              .forEach((key, value) -> user.setAttribute(token.getApplicationId() + "." + key, Collections.singletonList(String.valueOf(value))));
-        } catch (VerificationException ex) {
-            return tokenContext.getSession().getProvider(LoginFormsProvider.class)
-                    .setError(Messages.INVALID_PARAMETER)
-                    .createErrorPage(Response.Status.INTERNAL_SERVER_ERROR);
-        }
-
-        event.success();
-
-        user.removeRequiredAction(RedirectToExternalApplication.ID);
-        authSession.removeRequiredAction(RedirectToExternalApplication.ID);
-
-        // User updated. Now if authentication is not in progress (e.g. opened in a new browser window), just show an info that account has been updated
-        if (tokenContext.isAuthenticationSessionFresh()) {
-            AuthenticationSessionManager asm = new AuthenticationSessionManager(tokenContext.getSession());
-            asm.removeAuthenticationSession(tokenContext.getRealm(), authSession, true);
-            return tokenContext.getSession().getProvider(LoginFormsProvider.class)
-                    .setSuccess(Messages.ACCOUNT_UPDATED)
-                    .createInfoPage();
-        }
-
-        // Otherwise continue with next required action (if any)
-        String nextAction = AuthenticationManager.nextRequiredAction(tokenContext.getSession(), authSession, tokenContext.getClientConnection(), tokenContext.getRequest(), tokenContext.getUriInfo(), event);
-        return AuthenticationManager.redirectToRequiredActions(tokenContext.getSession(), tokenContext.getRealm(), authSession, tokenContext.getUriInfo(), nextAction);
     }
 
     @Override
